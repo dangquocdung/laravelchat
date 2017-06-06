b35c237a736a02b2a3658d7fcc79553c8fa1413b
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro.local>
2017-06-06 11:12:45 +0700

    ss

--- app/Http/Controllers/RedisController.php
+++ app/Http/Controllers/RedisController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Messages;
 
 class RedisController extends Controller
 {
     //
+
+    public function index(){
+      $messages = Messages::all();
+
+      return view ('messages',compact('messages'));
+
+    }
+
 }
