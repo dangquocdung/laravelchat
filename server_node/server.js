7153cc682d21f2ff00e21abdf8b55adaa4b34a61
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro.local>
2017-06-08 14:15:36 +0700

    ss

--- server_node/server.js
+++ server_node/server.js
@@ -1,1 +1,13 @@
-var io=
+var io = require('socketio')(6000)
+console.log('Connected to port 6000')
+
+io.on('error',function(socket){
+  console.log('error')
+})
+
+io.on('connection',function(socket){
+  console.log('Co nguoi vua ket noi' + socket.id)
+})
+
+var Redis = require('ioredis')
+var redis = new Redis(6379)
